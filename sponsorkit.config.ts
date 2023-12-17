70c23efe73c8a0f04d916096bfbe231207685989
Ian Becker <ian@beckr.dev>
2023-12-16 21:48:07 -0500

    fix(opencollective): removes hardcoded OC sponsors and enables sponsorkit fetching (#1)
    also updates the sponsorkit dependency to the new release with the fix

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,53 +1,5 @@
 import { BadgePreset, Sponsorship, defineConfig, presets } from 'sponsorkit'
 
-const OC_SPONSORSHIPS: Sponsorship[] = [
-  {
-    monthlyDollars: 300,
-    sponsor: {
-      avatarUrl: 'https://images.opencollective.com/appscreens/62776aa/avatar/256.png?height=256',
-      login: 'appscreens',
-      name: 'AppScreens',
-      type: 'Organization',
-      websiteUrl: 'https://appscreens.com/?_locale=en&utm_source=github&utm_medium=referral&utm_campaign=capawesome&gclid=capawesome',
-      linkUrl: 'https://opencollective.com/appscreens',
-    },
-    isOneTime: false,
-  },
-  {
-    monthlyDollars: 50,
-    sponsor: {
-      avatarUrl: 'https://images.opencollective.com/nfc21/9d1232a/logo/256.png?height=256',
-      login: 'nfc21',
-      name: 'NFC 21',
-      type: 'Organization',
-      websiteUrl: 'https://nfc21.de/',
-      linkUrl: 'https://opencollective.com/nfc21',
-    },
-    isOneTime: false
-  },
-  {
-    monthlyDollars: 50,
-    sponsor: {
-      avatarUrl: 'https://images.opencollective.com/erik-geers/avatar/256.png?height=256',
-      login: 'erikgeers',
-      name: 'Erik Geers',
-      type: 'User',
-      linkUrl: 'https://opencollective.com/erik-geers',
-    },
-    isOneTime: false
-  },
-  {
-    monthlyDollars: 500,
-    sponsor: {
-      avatarUrl: 'https://images.opencollective.com/colorful-casting/4e2c691/logo/256.png?height=256',
-      login: 'colorful-casting',
-      name: 'Colorful Casting',
-      type: 'Organization',
-      linkUrl: 'https://opencollective.com/colorful-casting',
-    },
-    isOneTime: false
-  }
-]
 
 const pastPreset: BadgePreset = {
   avatar: {
@@ -93,7 +45,7 @@ export default defineConfig({
     },
   ],
   onSponsorsFetched: (sponsors: Sponsorship[]) => {
-    sponsors = [...sponsors, ...OC_SPONSORSHIPS]
+    sponsors = [...sponsors]
     return sponsors
   }
 })
